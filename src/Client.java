d561a2fc2d0c74b1adf2e1e76f28e7a4db0b3982
PixelOverflow <bartek.dabrowski1.11@gmail.com>
2018-03-28 20:40:34 +0200

    Added networking logic, processing requests and an iterator bug

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,78 @@
-public class Client {
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
 
+public class Client {
+    private SocketChannel socketChannel;
+    private Selector selector;
+    private Charset charset = Charset.forName("UTF-8");
+    private String request;
+    private boolean isRunning;
+
+    public Client(String _host, int _port, String _request) {
+        InetSocketAddress socketAddress = new InetSocketAddress(_host, _port);
+        try {
+            socketChannel = SocketChannel.open(socketAddress);
+            socketChannel.configureBlocking(false);
+            selector = Selector.open();
+            socketChannel.register(selector, SelectionKey.OP_READ);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        request = _request;
+        System.out.println("Connected.");
+        serviceConnection();
+    }
+
+    private void serviceConnection() {
+        isRunning = true;
+
+        while (isRunning) {
+            try {
+                selector.select();
+                Set keys = selector.keys();
+                Iterator iter = keys.iterator();
+
+                while (iter.hasNext()) {
+                    SelectionKey key = (SelectionKey) iter.next();
+                    iter.remove();
+
+                    if (key.isReadable()) {
+                        SocketChannel sc = (SocketChannel) key.channel();
+                        sendRequest(sc);
+                    }
+                }
+                } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendRequest(SocketChannel sc) {
+        if (!sc.isOpen()) return;
+
+        try {
+            ByteBuffer buffer = charset.encode(request);
+            sc.write(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        isRunning = false;
+        System.out.println("Sent : "+request);
+    }
+
+    public static void main(String[] args) {
+        String request = "";
+        for (int i = 2; i <= args.length-1 ;++i) {
+            request += args[i]+" ";
+        }
+
+        Client client = new Client(args[0], Integer.parseInt(args[1]), request);
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,104 @@
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Server {
-    
+    private ServerSocketChannel socketchan;
+    private Selector selector;
+    private ByteBuffer buff = ByteBuffer.allocate(1024);
+    private Charset charset = Charset.forName("UTF-8");
+
+    public Server(String _host, int _port) {
+        InetSocketAddress serverAddress = new InetSocketAddress(_host, _port);
+        try {
+            socketchan = ServerSocketChannel.open();
+            socketchan.configureBlocking(false);
+            socketchan.bind(serverAddress);
+            selector = Selector.open();
+            socketchan.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("Server running.");
+        serviceConnections();
+    }
+
+    public void serviceConnections(){
+        boolean serverIsRunning = true;
+
+        while (serverIsRunning){
+            try {
+                selector.select();
+                Set keys = selector.keys();
+                Iterator iter = keys.iterator();
+
+                while (iter.hasNext()){
+                    SelectionKey  key = (SelectionKey) iter.next();
+                    iter.remove();
+                    if (key.isAcceptable()) {
+                        SocketChannel sc = socketchan.accept();
+                        sc.configureBlocking(false);
+                        continue;
+                    }
+                    if (key.isReadable()) {
+                        SocketChannel sc = (SocketChannel) key.channel();
+                        serviceRequest(sc);
+                        continue;
+                    }
+                    Thread.sleep(100);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void serviceRequest(SocketChannel sc) {
+        if (!sc.isOpen()) return;
+
+        try {
+            sc.read(buff);
+
+            String request = String.valueOf(charset.decode(buff));
+            String[] instructions = request.split(" ");
+            instructions[0].toLowerCase();
+
+            if (instructions.equals("echo")) {
+                System.out.println(request.substring(5));
+            }
+            else if (instructions.equals("calculate") || instructions.equals("calc")) {
+                double result = 0;
+                switch (instructions[2].charAt(0)) {
+                    case '+':
+                        result = Double.parseDouble(instructions[1]) + Double.parseDouble(instructions[3]);
+                        break;
+                    case '-':
+                        result = Double.parseDouble(instructions[1]) - Double.parseDouble(instructions[3]);
+                        break;
+                    case '*':
+                        result = Double.parseDouble(instructions[1]) * Double.parseDouble(instructions[3]);
+                        break;
+                    case '/':
+                        result = Double.parseDouble(instructions[1]) / Double.parseDouble(instructions[3]);
+                        break;
+                }
+
+                System.out.println(instructions[1]+" "+instructions[2]+" "+instructions[3]+" = "+result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(args[0], Integer.parseInt(args[1]));
+    }
 }
